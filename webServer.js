dc1933e47db980570e8f870425bada94464a6090
avi34 <64527129+avi34@users.noreply.github.com>
2020-05-09 22:46:57 +0530

    Create webServer.js

---
+++ webServer.js
@@ -0,0 +1,22 @@
+create a folder in which you want to set up project
+and run the nmp init -y command for installing npm pacakge 
+install express library (Fast, unopinionated, minimalist web framework for Node.js)
+make a folder src 
+make a file for starting point (app.js)
+
+
+const express = require('express');
+
+const app = express()
+app.get('',(req, res)=>{
+    res.send('hello express!')
+})
+app.get('/help',(req, res)=>{
+    res.send('this is help page')
+})
+//app.com
+//app.com/help
+//app.com/about
+app.listen(3000, ()=>{
+    console.log('server is up on post 3000')
+})
